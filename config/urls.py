79b499b92fd3606609e0ee605292021b54ca8275
prasutagus <dawheel@davidwheeler.me.uk>
2020-03-01 13:56:16 +0000

    Implement cheese list page, add navbar link.

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     # User management
     path("users/", include("everycheese.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
+    path("cheeses/", include("everycheese.cheeses.urls", namespace='cheeses')),
     # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
