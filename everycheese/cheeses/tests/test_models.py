184b9bf3291592d1e2e810f69546deaf041ca767
David Wheeler <dawheel@davidwheeler.me.uk>
2020-03-10 12:48:15 +0000

    Add test for get_absolute_url() method

--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -11,3 +11,7 @@ def test___str__():
 
     assert cheese.__str__() == cheese.name
 
+def test_get_absolute_url():
+    cheese = CheeseFactory()
+    url = cheese.get_absolute_url()
+    assert url == f'/cheeses/{cheese.slug}/'
